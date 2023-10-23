dd218289b685ca78e3df83ab3de3cbf50887729c
Valeria Piscopo <s303313@studenti.polito.it>
2023-10-23 09:41:23 +0200

    XOR gate

---
+++ src/basic_components/XOR.sv
@@ -0,0 +1,11 @@
+module XOR (
+     input logic   in1,		       
+     input logic   in2,
+     output logic  out   	       
+   );
+
+   assign out = in1^in2;
+
+endmodule : XOR
+
+// INSERT A DELAY?
