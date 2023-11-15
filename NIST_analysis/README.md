be203bfd7c11f8926a62e975e17bff8d8878a284
Valeria Piscopo <s303313@studenti.polito.it>
2023-11-15 14:33:12 +0100

    Scripts for NIST test suites output files analysis

--- NIST_analysis/README.md
+++ NIST_analysis/README.md
@@ -1,2 +1,5 @@
-"analysis_res.py" can be used to analyze the output file of the NIST test suite 
-(git ref: terrillmoore/NIST-Statistical-Test-Suite)
+---> "analysis_NIST.py" can be used to analyze the output file of the NIST test suite 
+     (git ref: terrillmoore/NIST-Statistical-Test-Suite)
+
+---> "analysis_NIST_90B.py" can be used to analyze the output file of the NIST SP 800-90B test suite 
+     (git ref: usnistgov/SP800-90B_EntropyAssessment)
---
+++ NIST_analysis/analysis_NIST_90B.py
@@ -0,0 +1,51 @@
+##############################################################################
+#                                                                            #
+#   This script analyzes the output files from NIST SP 800-90B test and      #
+#   produces a text file where different results for different analyses      #
+#   are compared                                                             #
+#                                                                            #
+##############################################################################
+import sys
+import os.path
+import math
+import numpy as np
+import matplotlib.pyplot as plot
+import glob
+
+
+path = os.path.join(os.getcwd(), "NON_IID_results/*.txt")
+files = glob.glob(path)
+s_to_find = 'H_original: '
+H_vals = []
+
+for elem in files:
+    with open(elem, 'r') as fileID:
+        for i, line in enumerate(fileID):
+            if s_to_find in line:
+               H_vals.append(line.split()[1])
+            
+
+new_file = os.path.join(os.getcwd(), "NON_IID_results/Comparation.txt")
+files.remove(new_file)
+with open(new_file, 'w') as fileID:
+    fileID.write("Min entropy  |  Case of study\n")
+    fileID.write("---------------------------------------------------------------\n")
+    for i, elem in enumerate(H_vals):
+        fileID.write(elem)
+        for n in range(13-len(elem)):
+            fileID.write(" ")
+        fileID.write("| ")
+        file_name = files[i].split('/')[-1]
+        n_INVs = (int)(file_name.split('_')[3].split('I')[0])
+        n_ROs  = (int)(file_name.split('_')[4].split('R')[0])
+        sigma  = (int)(file_name.split('_')[5].split('s')[0])
+        fileID.write("n_RO = %d, n_INV = %d, sigma = %d ps \n" %(n_ROs, n_INVs, sigma))
+        
+        if(elem == max(H_vals)):
+            n_INV_max = n_INVs
+            n_RO_max  = n_ROs
+            sigma_max = sigma
+            
+    fileID.write("---------------------------------------------------------------\n")
+    fileID.write("Best result:\n")
+    fileID.write("H = %f - %d parallel ROs of %d inverters (sigma = %d ps)" %(float(max(H_vals)), n_RO_max, n_INV_max, sigma_max))
