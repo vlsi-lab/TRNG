2f3ec23517ecf4380d08ade806b6ef9665903ce0
Valeria Piscopo <s303313@studenti.polito.it>
2023-11-16 13:42:12 +0100

    Update script for NIST SP 800-90B output analysis

--- NIST_analysis/analysis_NIST_90B.py
+++ NIST_analysis/analysis_NIST_90B.py
@@ -28,13 +28,22 @@ for elem in files:
 new_file = os.path.join(os.getcwd(), "NON_IID_results/Comparation.txt")
 files.remove(new_file)
 with open(new_file, 'w') as fileID:
-    fileID.write("Min entropy  |  Case of study\n")
-    fileID.write("---------------------------------------------------------------\n")
+    fileID.write("Min entropy  |   Shannon entropy   |  Case of study\n")
+    fileID.write("--------------------------------------------------------------------------\n")
     for i, elem in enumerate(H_vals):
+
         fileID.write(elem)
         for n in range(13-len(elem)):
             fileID.write(" ")
         fileID.write("| ")
+
+        float_elem = float(elem)
+        Shannon_H = -2**(-float_elem)*math.log2(2**(-float_elem)) - (1-2**(-float_elem))*math.log2(1-2**(-float_elem))
+        fileID.write(str(Shannon_H))
+        for n in range(20-len(str(Shannon_H))):
+            fileID.write(" ")
+        fileID.write("| ")
+
         file_name = files[i].split('/')[-1]
         n_INVs = (int)(file_name.split('_')[3].split('I')[0])
         n_ROs  = (int)(file_name.split('_')[4].split('R')[0])
@@ -42,10 +51,11 @@ with open(new_file, 'w') as fileID:
         fileID.write("n_RO = %d, n_INV = %d, sigma = %d ps \n" %(n_ROs, n_INVs, sigma))
         
         if(elem == max(H_vals)):
-            n_INV_max = n_INVs
-            n_RO_max  = n_ROs
-            sigma_max = sigma
+            n_INV_max  = n_INVs
+            n_RO_max   = n_ROs
+            sigma_max  = sigma
+            Shan_H_max = Shannon_H
             
-    fileID.write("---------------------------------------------------------------\n")
+    fileID.write("--------------------------------------------------------------------------\n")
     fileID.write("Best result:\n")
-    fileID.write("H = %f - %d parallel ROs of %d inverters (sigma = %d ps)" %(float(max(H_vals)), n_RO_max, n_INV_max, sigma_max))
+    fileID.write("H_min = %f, H_shan = %f - %d ROs of %d inverters (sigma = %d ps)" %(float(max(H_vals)), Shan_H_max, n_RO_max, n_INV_max, sigma_max))
