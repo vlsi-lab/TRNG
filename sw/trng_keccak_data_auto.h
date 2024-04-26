dd8ecbd2ee8c796b8964e436a829ce27fcc8e278
Valeria-Piscopo <66244787+Valeria-Piscopo@users.noreply.github.com>
2024-04-26 15:52:43 +0200

    Update trng_keccak_data_auto.h

---
+++ sw/trng_keccak_data_auto.h
@@ -0,0 +1,329 @@
+// Generated register defines for trng_keccak_data
+
+#ifndef _TRNG_KECCAK_DATA_REG_DEFS_
+#define _TRNG_KECCAK_DATA_REG_DEFS_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Register width
+#define TRNG_KECCAK_DATA_PARAM_REG_WIDTH 32
+
+// Subword of input of Keccak module (common parameters)
+#define TRNG_KECCAK_DATA_KECCAK_DIN_KECCAK_DIN_FIELD_WIDTH 32
+#define TRNG_KECCAK_DATA_KECCAK_DIN_KECCAK_DIN_FIELDS_PER_REG 1
+#define TRNG_KECCAK_DATA_KECCAK_DIN_MULTIREG_COUNT 50
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_0_REG_OFFSET 0x0
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_1_REG_OFFSET 0x4
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_2_REG_OFFSET 0x8
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_3_REG_OFFSET 0xc
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_4_REG_OFFSET 0x10
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_5_REG_OFFSET 0x14
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_6_REG_OFFSET 0x18
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_7_REG_OFFSET 0x1c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_8_REG_OFFSET 0x20
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_9_REG_OFFSET 0x24
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_10_REG_OFFSET 0x28
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_11_REG_OFFSET 0x2c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_12_REG_OFFSET 0x30
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_13_REG_OFFSET 0x34
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_14_REG_OFFSET 0x38
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_15_REG_OFFSET 0x3c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_16_REG_OFFSET 0x40
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_17_REG_OFFSET 0x44
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_18_REG_OFFSET 0x48
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_19_REG_OFFSET 0x4c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_20_REG_OFFSET 0x50
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_21_REG_OFFSET 0x54
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_22_REG_OFFSET 0x58
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_23_REG_OFFSET 0x5c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_24_REG_OFFSET 0x60
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_25_REG_OFFSET 0x64
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_26_REG_OFFSET 0x68
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_27_REG_OFFSET 0x6c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_28_REG_OFFSET 0x70
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_29_REG_OFFSET 0x74
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_30_REG_OFFSET 0x78
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_31_REG_OFFSET 0x7c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_32_REG_OFFSET 0x80
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_33_REG_OFFSET 0x84
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_34_REG_OFFSET 0x88
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_35_REG_OFFSET 0x8c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_36_REG_OFFSET 0x90
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_37_REG_OFFSET 0x94
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_38_REG_OFFSET 0x98
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_39_REG_OFFSET 0x9c
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_40_REG_OFFSET 0xa0
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_41_REG_OFFSET 0xa4
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_42_REG_OFFSET 0xa8
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_43_REG_OFFSET 0xac
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_44_REG_OFFSET 0xb0
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_45_REG_OFFSET 0xb4
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_46_REG_OFFSET 0xb8
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_47_REG_OFFSET 0xbc
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_48_REG_OFFSET 0xc0
+
+// Subword of input of Keccak module
+#define TRNG_KECCAK_DATA_KECCAK_DIN_49_REG_OFFSET 0xc4
+
+// Subword of Keccak results (common parameters)
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_KECCAK_DOUT_FIELD_WIDTH 32
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_KECCAK_DOUT_FIELDS_PER_REG 1
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_MULTIREG_COUNT 50
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_0_REG_OFFSET 0xc8
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_1_REG_OFFSET 0xcc
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_2_REG_OFFSET 0xd0
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_3_REG_OFFSET 0xd4
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_4_REG_OFFSET 0xd8
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_5_REG_OFFSET 0xdc
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_6_REG_OFFSET 0xe0
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_7_REG_OFFSET 0xe4
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_8_REG_OFFSET 0xe8
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_9_REG_OFFSET 0xec
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_10_REG_OFFSET 0xf0
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_11_REG_OFFSET 0xf4
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_12_REG_OFFSET 0xf8
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_13_REG_OFFSET 0xfc
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_14_REG_OFFSET 0x100
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_15_REG_OFFSET 0x104
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_16_REG_OFFSET 0x108
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_17_REG_OFFSET 0x10c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_18_REG_OFFSET 0x110
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_19_REG_OFFSET 0x114
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_20_REG_OFFSET 0x118
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_21_REG_OFFSET 0x11c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_22_REG_OFFSET 0x120
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_23_REG_OFFSET 0x124
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_24_REG_OFFSET 0x128
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_25_REG_OFFSET 0x12c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_26_REG_OFFSET 0x130
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_27_REG_OFFSET 0x134
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_28_REG_OFFSET 0x138
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_29_REG_OFFSET 0x13c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_30_REG_OFFSET 0x140
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_31_REG_OFFSET 0x144
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_32_REG_OFFSET 0x148
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_33_REG_OFFSET 0x14c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_34_REG_OFFSET 0x150
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_35_REG_OFFSET 0x154
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_36_REG_OFFSET 0x158
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_37_REG_OFFSET 0x15c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_38_REG_OFFSET 0x160
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_39_REG_OFFSET 0x164
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_40_REG_OFFSET 0x168
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_41_REG_OFFSET 0x16c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_42_REG_OFFSET 0x170
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_43_REG_OFFSET 0x174
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_44_REG_OFFSET 0x178
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_45_REG_OFFSET 0x17c
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_46_REG_OFFSET 0x180
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_47_REG_OFFSET 0x184
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_48_REG_OFFSET 0x188
+
+// Subword of Keccak results
+#define TRNG_KECCAK_DATA_KECCAK_DOUT_49_REG_OFFSET 0x18c
+
+// Random key
+#define TRNG_KECCAK_DATA_TRNG_DOUT_REG_OFFSET 0x190
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif  // _TRNG_KECCAK_DATA_REG_DEFS_
+// End generated register defines for trng_keccak_data
